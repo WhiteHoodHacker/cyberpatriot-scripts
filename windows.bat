00d07267cfc00b29ad951827f395bb2887ed8bea
WhiteHoodHacker <TheWhiteHoodHacker@gmail.com>
2020-12-02 19:02:25 -0600

    fixed an issue with prohibited media files
    because rick rolls aren't prohibited

---
+++ windows.bat
@@ -0,0 +1,56 @@
+@echo off
+cls
+
+REM I hecking told you...
+
+echo "We're no strangers to love"
+echo "You know the rules and so do I"
+echo "A full commitment's what I'm thinking of"
+echo "You wouldn't get this from any other guy"
+echo.
+echo "I just wanna tell you how I'm feeling"
+echo "Gotta make you understand"
+echo.
+echo "Never gonna give you up"
+echo "Never gonna let you down"
+echo "Never gonna run around and desert you"
+echo "Never gonna make you cry"
+echo "Never gonna say goodbye"
+echo "Never gonna tell a lie and hurt you"
+echo.
+echo "We've known each other for so long"
+echo "Your heart's been aching but you're too shy to say it"
+echo "Inside we both know what's been going on"
+echo "We know the game and we're gonna play it"
+echo.
+echo "And if you ask me how I'm feeling"
+echo "Don't tell me you're too blind to see"
+echo.
+echo "Never gonna give you up"
+echo "Never gonna let you down"
+echo "Never gonna run around and desert you"
+echo "Never gonna make you cry"
+echo "Never gonna say goodbye"
+echo "Never gonna tell a lie and hurt you"
+echo.
+echo "No, I'm never gonna give you up"
+echo "No, I'm never gonna let you down"
+echo "No, I'll never run around and hurt you"
+echo "Never, ever desert you"
+echo.
+echo "We've known each other for so long"
+echo "Your heart's been aching but"
+echo.
+echo "Never gonna give you up"
+echo "Never gonna let you down"
+echo "Never gonna run around and desert you"
+echo "Never gonna make you cry"
+echo "Never gonna say goodbye"
+echo "Never gonna tell a lie and hurt you"
+echo.
+echo "No, I'm never gonna give you up"
+echo "No, I'm never gonna let you down"
+echo "No, I'll never run around and hurt you"
+echo "I'll never, ever desert you"
+
+pause
